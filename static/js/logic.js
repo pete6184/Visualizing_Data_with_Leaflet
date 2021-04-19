123c0bae6db95a0f92cd476d7ed97d3350c6c19e
Peter Johnson <74940976+pete6184@users.noreply.github.com>
2021-04-19 01:03:37 -0700

    checkpoint save - working on the legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -75,7 +75,11 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(myMap);
 
 // Create legend
-
+const legend = L.control({ position: "bottomright"});
+legend.onAdd = function () {
+    const div = L.DomUtil.create("div", "info legend");
+    
+}
 
 
 
